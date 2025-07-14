fffff758e2f9386cb43f69840666fbf4fd4d16d2
Aidan Fischer <aidanfischer@gmail.com>
2025-07-13 21:38:57 -0500

    update

--- databases/schemas/roomlogs.sql
+++ databases/schemas/roomlogs.sql
@@ -4,11 +4,13 @@ CREATE TABLE public.roomlogs (
 	userid text NULL,
 	time TIMESTAMP(6) NOT NULL,
 	log text NOT NULL,
-	INDEX linecount (userid, roomid, time),
-	INDEX month (roomid, time),
-	INDEX type (roomid, type, time),
-	INDEX rename_idx (roomid)
 );
+
+CREATE INDEX linecount ON roomlogs(userid, roomid, time);
+CREATE INDEX month ON roomlogs(roomid, time);
+CREATE INDEX type ON roomlogs(roomid, type, time);
+CREATE INDEX rename_idx ON roomlogs(roomid);
+
 -- computed columns have to be added after apparently
 ALTER TABLE roomlogs ADD COLUMN content TSVECTOR GENERATED ALWAYS AS (to_tsvector('english', log)) STORED;
 
