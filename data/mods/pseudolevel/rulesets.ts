96a3e79721070fc42cecf8c347cacb86f31b8ceb
Aidan Fischer <aidanfischer@gmail.com>
2025-09-07 19:23:51 -0500

    uhhh

--- data/mods/pseudolevel/rulesets.ts
+++ data/mods/pseudolevel/rulesets.ts
@@ -2,7 +2,7 @@ export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable 
     standard: {
         effectType: 'ValidatorRule',
         name: "Standard",
-        ruleset: ["Ionite Metagame One Restricted Mon", "Sleep Clause Mod"]
+        ruleset: ["ioniterestrictmon", "Sleep Clause Mod"]
     },
 
 }
