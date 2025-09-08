bae6f31156ae03022468a939195012a45996b0f1
Aidan Fischer <aidanfischer@gmail.com>
2025-09-07 19:18:27 -0500

    uh

--- data/mods/pseudolevel/rulesets.ts
+++ data/mods/pseudolevel/rulesets.ts
@@ -1,23 +1,8 @@
 export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable = {
-    onerestrictmon: {
-        effectType: 'Rule',
-        name: "Metagame One Restricted Mon",
-        onValidateTeam(team){
-            let hasRestricted = false;
-            for (const set of team) {
-                if(this.dex.species.get(set.species).tags.includes("Ionite - Restricted")){
-                    if(hasRestricted) {
-                        return ["Hey now don't be cheeky. 1 restricted Pokémon only."];
-                    }
-                    hasRestricted = true;
-                }
-            }
-        }
-    },
-        standard: {
+    standard: {
         effectType: 'ValidatorRule',
         name: "Standard",
-        ruleset: ["Metagame One Restricted Mon", "Sleep Clause Mod"]
+        ruleset: ["Ionite Metagame One Restricted Mon", "Sleep Clause Mod"]
     },
 
 }
--- data/rulesets.ts
+++ data/rulesets.ts
@@ -3064,4 +3064,19 @@ export const Rulesets: import('../sim/dex-formats').FormatDataTable = {
 			}
 		},
 	},
+	ioniterestrictmon: {
+        effectType: 'Rule',
+        name: "Ionite Metagame One Restricted Mon",
+        onValidateTeam(team){
+            let hasRestricted = false;
+            for (const set of team) {
+                if(this.dex.species.get(set.species).tags.includes("Ionite - Restricted")){
+                    if(hasRestricted) {
+                        return ["Hey now don't be cheeky. 1 restricted Pokémon only."];
+                    }
+                    hasRestricted = true;
+                }
+            }
+        }
+    },
 };
