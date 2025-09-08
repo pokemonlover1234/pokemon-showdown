9764d9c95d88311b726c73b6392a900df48e00ef
Aidan Fischer <aidanfischer@gmail.com>
2025-09-07 19:18:27 -0500

    uh

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -3228,4 +3228,19 @@ export const Rulesets: import('../sim/dex-formats').FormatDataTable = {
 			if (!speciesMods.length) throw new Error('This format has no rules that modify base stats.');
 		},
 	},
+	ioniterestrictmon: {
+        effectType: 'Rule',
+        name: "Ionite Metagame One Restricted Mon",
+        onValidateTeam(team){
+            let hasRestricted = false;
+            for (const set of team) {
+                if(this.dex.species.get(set.species).tags.includes("Ionite - Restricted")){
+                    if(hasRestricted) {
+                        return ["Hey now don't be cheeky. 1 restricted Pokémon only."];
+                    }
+                    hasRestricted = true;
+                }
+            }
+        }
+    },
 };
