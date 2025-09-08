97bb3f23dcd5f6ee62c1eeb6a5edc6b54c81447e
Aidan Fischer <aidanfischer@gmail.com>
2025-09-07 20:02:31 -0500

    fine then. use gen 9 at least

--- config/formats.ts
+++ config/formats.ts
@@ -3457,7 +3457,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 	},
 	{
 		name: "Pseudo Level",
-		mod: "pseudolevel",
+		mod: "gen9pseudolevel",
 		ruleset: ["Standard"]
 	},
 
