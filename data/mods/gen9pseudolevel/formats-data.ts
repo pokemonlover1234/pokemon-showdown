5eaffa3bf92571d407aad968bcaba7fb9e28c830
Aidan Fischer <aidanfischer@gmail.com>
2025-09-22 19:07:19 -0500

    fix alt form tiers

--- data/mods/gen9pseudolevel/formats-data.ts
+++ data/mods/gen9pseudolevel/formats-data.ts
@@ -1,9 +1,7 @@
 import data from "./data.json";
-import { FormatsData as VanillaData } from "../../formats-data";
-const gennedTable: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable =
-JSON.parse(JSON.stringify(VanillaData));
-
-for (const pokemon of Object.keys(gennedTable)) {
+import { Pokedex as VanillaDex } from "../../pokedex";
+const gennedTable: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable = {};
+for (const pokemon of Object.keys(VanillaDex)) {
 	try {
 		if (pokemon in data) {
 			gennedTable[pokemon as keyof typeof gennedTable] = {
