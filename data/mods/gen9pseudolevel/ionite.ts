088c0b53449b035da94effaccac978e12862c595
Aidan Fischer <aidanfischer@gmail.com>
2025-09-24 23:03:52 -0500

    metagame caching fixed

--- data/mods/gen9pseudolevel/ionite.ts
+++ data/mods/gen9pseudolevel/ionite.ts
@@ -7,32 +7,32 @@ function buildCache() {
 	const formatsdataout = formatsdata(data);
 	const learnsetdataout = learnsets(data);
 	const pokedexdataout = pokedex(data);
-	fs.writeFileSync("./formats-data-cache.json", JSON.stringify(formatsdataout, null, 2), 'utf8');
-	fs.writeFileSync("./learnsets-cache.json", JSON.stringify(learnsetdataout, null, 2), 'utf8');
-	fs.writeFileSync("./pokedex-cache.json", JSON.stringify(pokedexdataout, null, 2), 'utf8');
+	fs.writeFileSync(__dirname + "/formats-data-cache.json", JSON.stringify(formatsdataout, null, 2), 'utf8');
+	fs.writeFileSync(__dirname + "/learnsets-cache.json", JSON.stringify(learnsetdataout, null, 2), 'utf8');
+	fs.writeFileSync(__dirname + "/pokedex-cache.json", JSON.stringify(pokedexdataout, null, 2), 'utf8');
 	dirty.dirty = false;
-	fs.writeFileSync("./dirty.json", JSON.stringify(dirty, null, 2));
+	fs.writeFileSync(__dirname + "/dirty.json", JSON.stringify(dirty, null, 2));
 }
 
 function formatsDataTable() {
 	if (dirty.dirty) {
 		buildCache();
 	}
-	return JSON.parse(fs.readFileSync("./formats-data-cache.json", 'utf8'));
+	return JSON.parse(fs.readFileSync(__dirname + "/formats-data-cache.json", 'utf8'));
 }
 
 function learnsetTable() {
 	if (dirty.dirty) {
 		buildCache();
 	}
-	return JSON.parse(fs.readFileSync("./learnsets-cache.json", 'utf8'));
+	return JSON.parse(fs.readFileSync(__dirname + "/learnsets-cache.json", 'utf8'));
 }
 
 function pokedexTable() {
 	if (dirty.dirty) {
 		buildCache();
 	}
-	return JSON.parse(fs.readFileSync("./pokedex-cache.json", 'utf8'));
+	return JSON.parse(fs.readFileSync(__dirname + "/pokedex-cache.json", 'utf8'));
 }
 
 export default { "formats-data": formatsDataTable, "learnsets": learnsetTable, "pokedex": pokedexTable };
