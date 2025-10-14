5e94ed5ffdf3558096ead321dc54e2cd931d9b68
Aidan Fischer <aidanfischer@gmail.com>
2025-10-14 18:47:24 -0500

    okay so the move types are case sensitive got it this is fine

--- data/moves.ts
+++ data/moves.ts
@@ -22154,7 +22154,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		priority: 0,
 		flags: { metronome: 1, mirror: 1, protect: 1 },
 		target: 'normal',
-		type: 'flying',
+		type: 'Flying',
 		contestType: 'Cool',
 	},
 	gleamstep: {
@@ -22168,7 +22168,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		flags: { metronome: 1, mirror: 1, protect: 1 },
 		selfSwitch: true,
 		target: 'normal',
-		type: 'fairy',
+		type: 'Fairy',
 		contestType: "Beatiful",
 		onBasePower(power, source) {
 			if (source.baseStoredStats.atk > source.baseStoredStats.spa) {
@@ -22189,7 +22189,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 			if (type === 'Ghost') return 1;
 		},
 		target: 'normal',
-		type: 'bug',
+		type: 'Bug',
 	},
 	rustyblade: {
 		accuracy: 90,
@@ -22201,7 +22201,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		flags: { metronome: 1, mirror: 1, protect: 1, contact: 1 },
 		category: "Physical",
 		target: "normal",
-		type: "poison",
+		type: "Poison",
 		onAfterHit(target, source, move) {
 			if (!move.hasSheerForce && source.hp) {
 				for (const side of source.side.foeSidesWithConditions()) {
@@ -22227,7 +22227,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		priority: 0,
 		flags: { metronome: 1, mirror: 1, protect: 1, reflectable: 1 },
 		target: "normal",
-		type: "ice",
+		type: "Ice",
 		status: 'frostbite',
 	},
 	loosedirt: {
@@ -22244,7 +22244,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		priority: 0,
 		flags: { metronome: 1, mirror: 1, protect: 1 },
 		target: "normal",
-		type: "ground",
+		type: "Ground",
 	},
 	burningcoals: {
 		accuracy: true,
