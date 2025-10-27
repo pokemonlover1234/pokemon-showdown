5af90e6b0caa31bc234b2146fb1dcceb550266c6
Aidan Fischer <aidanfischer@gmail.com>
2025-10-26 21:05:40 -0500

    Meteor easter egg + success is not permanent

--- data/mods/gen9pseudolevel/formats-data.ts
+++ data/mods/gen9pseudolevel/formats-data.ts
@@ -1,3 +1,4 @@
 import datatables from "./ionite";
 
+// eslint-disable-next-line @stylistic/max-len
 export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable = datatables["formats-data"]();
