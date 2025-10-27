39d75af8c40d7f5dc5134c9e6e1ed2c500b04897
Aidan Fischer <aidanfischer@gmail.com>
2025-10-26 21:05:40 -0500

    Meteor easter egg + success is not permanent

--- data/items.ts
+++ data/items.ts
@@ -8227,10 +8227,7 @@ export const Items: import('../sim/dex-items').ItemDataTable = {
 				for (const pokemon of side.active) {
 					if (!pokemon) continue;
 					if (!pokemon.volatiles["pandorascurse"] && pokemon.hp) {
-						if (pokemon.isProtected()) {
-							this.add("-activate", pokemon, "move: Protect");
-							continue;
-						}
+						success = false;
 						pokemon.addVolatile("pandorascurse", target);
 						const curse = pokemon.volatiles["pandorascurse"];
 						console.log("Rolling status for " + pokemon.name);
