7e02a22da2ddf5f68702a04add89ddd95332e1df
Aidan Fischer <aidanfischer@gmail.com>
2026-01-18 10:11:56 -0600

    dark rock sprite

--- data/items.ts
+++ data/items.ts
@@ -8259,7 +8259,7 @@ export const Items: import('../sim/dex-items').ItemDataTable = {
 	},
 	speedorb: {
 		name: "Speed Orb",
-		spritenum: 3,
+		spritenum: 2,
 		fling: {
 			basePower: 60,
 		},
@@ -8275,6 +8275,7 @@ export const Items: import('../sim/dex-items').ItemDataTable = {
 	// Actual functionality handled by Nighttime weather
 	darkrock: {
 		name: "Dark Rock",
+		spritenum: 3,
 		fling: {
 			basePower: 60,
 		},
