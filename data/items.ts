bd6ead8935758abca597ffcbb9d276aa280eaa34
ThatOneStorm <shagmenot@gmail.com>
2026-04-20 19:45:27 -0400

    Update items.ts

--- data/items.ts
+++ data/items.ts
@@ -8142,6 +8142,18 @@ export const Items: import('../sim/dex-items').ItemDataTable = {
 		gen: 6,
 		isNonstandard: "CAP",
 	},
+	machampite: {
+		name: "Machampite",
+		spritenum: 576,
+		megaStone: { "Machamp": "Machamp-Mega" },
+		itemUser: ["Machamp"],
+		onTakeItem(item, source) {
+			return !item.megaStone?.[source.baseSpecies.baseSpecies];
+		},
+		num: 677,
+		gen: 6,
+		isNonstandard: "CAP",
+	},
 	vilevial: {
 		name: "Vile Vial",
 		spritenum: 752,
