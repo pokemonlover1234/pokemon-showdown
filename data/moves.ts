c19bac9ebc7f21eea33da514b439f394dfc70c15
Aidan Fischer <aidanfischer@gmail.com>
2026-05-02 18:33:03 -0500

    oops

--- data/moves.ts
+++ data/moves.ts
@@ -21825,7 +21825,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 			},
 			onAnyHit(target, source, move) {
 				// runImmunity returns true if not immune
-				if (target !== source && this.effectState.target.hasAlly(target) &&
+				if (target && source && target !== source && this.effectState.target.hasAlly(target) &&
 					target.runEffectiveness(move) < 0 && target.runImmunity(move) && !target.fainted) {
 					const damage = target.heal(Math.floor(target.maxhp / 4));
 					if (damage) {
