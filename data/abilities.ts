77129896469845a0ee2b32db67ef32d0c8c4f340
Aidan Fischer <aidanfischer@gmail.com>
2026-05-04 22:08:24 -0500

    full force nonstop

--- data/abilities.ts
+++ data/abilities.ts
@@ -6100,4 +6100,17 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			}
 		},
 	},
+	fullforcenonstop: {
+		isNonstandard: "Custom",
+		flags: {},
+		name: "Full Force Non-Stop",
+		rating: 3,
+		desc: "User doesn't recharge but takes 33% recoil damage after using a damaging recharge move. ",
+		onTryAddVolatile(status, target, source, sourceEffect) {
+			if (status.id === 'mustrecharge') {
+				this.damage(target.maxhp / 3, target, target, this.dex.abilities.get('Full Force Non-Stop'))
+				return false;
+			}
+		},
+	}
 };
