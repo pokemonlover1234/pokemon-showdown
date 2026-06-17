5360468beb90f3e107dc614966e3efd1d7639771
Miracle-Bee <missroleplaygamer@gmail.com>
2026-06-17 19:05:36 -0400

    Constellera specific move edits

--- data/mods/gen9constellera/moves.ts
+++ data/mods/gen9constellera/moves.ts
@@ -15,4 +15,70 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		type: "Astral",
 		desc: "Hits Twice",
 	},
+	icebeam: {
+		inherit: true,
+		secondary: {
+			chance: 10,
+			status: 'frostbite',
+		},
+	},
+	blizzard: {
+		inherit: true,
+		secondary: {
+			chance: 10,
+			status: 'frostbite',
+		},
+	},
+	powdersnow: {
+		inherit: true,
+		secondary: {
+			chance: 10,
+			status: 'frostbite',
+		},
+	},
+	freezeshock: {
+		inherit: true,
+		secondary: {
+			chance: 30,
+			onHit(target, source) {
+				const status = this.sample(['par', 'frz']);
+				target.trySetStatus(status, source);
+			},
+		},
+	},
+	iceburn: {
+		inherit: true,
+		secondary: {
+			chance: 30,
+			onHit(target, source) {
+				const status = this.sample(['brn', 'frostbite']);
+				target.trySetStatus(status, source);
+			},
+		},
+	},
+	glaciate: {
+		inherit: true,
+		secondary: {
+			chance: 20,
+			onHit(target, source) {
+				const status = this.sample(['frz', 'frostbite']);
+				target.trySetStatus(status, source);
+			},
+		},
+	},
+	cryofreeze: {
+		inherit: true,
+		accuracy: 75,
+	},
+	mountaingale: {
+		inherit: true,
+		secondary: {
+			chance: 10,
+			status: 'frz',
+		},
+	},
+
+	
+	
+	
 };
