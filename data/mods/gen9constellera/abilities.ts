7b215bc23e7c0e595cdc77551b1ad8ee34554a23
Miracle-Bee <missroleplaygamer@gmail.com>
2026-06-18 17:48:23 -0400

    Update abilities.ts
    Added reworks for Overgrow, Blaze, Torrent, Transistor, Dragon's Maw, Rocky Payload, Swarm, and Steelworker

--- data/mods/gen9constellera/abilities.ts
+++ data/mods/gen9constellera/abilities.ts
@@ -14,4 +14,237 @@ export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTa
 		rating: 3.5,
     num: 195
 	},
+	
+	overgrow: {
+		inherit: false,
+		onModifyAtkPriority: 5,
+		onModifyAtk(atk, attacker, defender, move) {
+			if (move.type === 'Grass' && source.hasType('Grass')) {
+				this.debug('Overgrow 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Grass') {
+				this.debug('Overgrow 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA(atk, attacker, defender, move) {
+			if (move.type === 'Grass' && source.hasType('Grass')) {
+				this.debug('Overgrow 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Grass') {
+				this.debug('Overgrow 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		flags: {},
+		name: "Overgrow",
+		rating: 2,
+		num: 65,
+	},
+	blaze: {
+		inherit: false,
+		onModifyAtkPriority: 5,
+		onModifyAtk(atk, attacker, defender, move) {
+			if (move.type === 'Fire' && source.hasType('Fire')) {
+				this.debug('Blaze 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Fire') {
+				this.debug('Blaze 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA(atk, attacker, defender, move) {
+			if (move.type === 'Fire' && source.hasType('Fire')) {
+				this.debug('Blaze 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Fire') {
+				this.debug('Blaze 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		flags: {},
+		name: "Blaze",
+		rating: 2,
+		num: 66,
+	},
+	torrent: {
+		inherit: false,
+		onModifyAtkPriority: 5,
+		onModifyAtk(atk, attacker, defender, move) {
+			if (move.type === 'Water' && source.hasType('Water')) {
+				this.debug('Torrent 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Water') {
+				this.debug('Torrent 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA(atk, attacker, defender, move) {
+			if (move.type === 'Water' && source.hasType('Water')) {
+				this.debug('Torrent 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Water') {
+				this.debug('Torrent 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		flags: {},
+		name: "Torrent",
+		rating: 2,
+		num: 67,
+	},
+	swarm: {
+		inherit: false,
+		onModifyAtkPriority: 5,
+		onModifyAtk(atk, attacker, defender, move) {
+			if (move.type === 'Bug' && source.hasType('Bug')) {
+				this.debug('Swarm 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Bug') {
+				this.debug('Swarm 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA(atk, attacker, defender, move) {
+			if (move.type === 'Bug' && source.hasType('Bug')) {
+				this.debug('Swarm 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Bug') {
+				this.debug('Swarm 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		flags: {},
+		name: "Swarm",
+		rating: 2,
+		num: 68,
+	},
+	transistor: {
+		inherit: false,
+		onModifyAtkPriority: 5,
+		onModifyAtk(atk, attacker, defender, move) {
+			if (move.type === 'Electric' && source.hasType('Electric')) {
+				this.debug('Transistor 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Electric') {
+				this.debug('Transistor 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA(atk, attacker, defender, move) {
+			if (move.type === 'Electric' && source.hasType('Electric')) {
+				this.debug('Transistor 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Electric') {
+				this.debug('Transistor 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		flags: {},
+		name: "Transistor",
+		rating: 2,
+		num: 262,
+	},
+	dragonsmaw: {
+		inherit: false,
+		onModifyAtkPriority: 5,
+		onModifyAtk(atk, attacker, defender, move) {
+			if (move.type === 'Dragon' && source.hasType('Dragon')) {
+				this.debug('Dragon\'s Maw 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Dragon') {
+				this.debug('Dragon\'s Maw 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA(atk, attacker, defender, move) {
+			if (move.type === 'Dragon' && source.hasType('Dragon')) {
+				this.debug('Dragon\'s Maw 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Dragon') {
+				this.debug('Dragon\'s Maw 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		flags: {},
+		name: "Dragon\'s Maw",
+		rating: 2,
+		num: 263,
+	},
+	rockypayload: {
+		inherit: false,
+		onModifyAtkPriority: 5,
+		onModifyAtk(atk, attacker, defender, move) {
+			if (move.type === 'Rock' && source.hasType('Rock')) {
+				this.debug('Rocky Payload 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Rock') {
+				this.debug('Rocky Payload 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA(atk, attacker, defender, move) {
+			if (move.type === 'Rock' && source.hasType('Rock')) {
+				this.debug('Rocky Payload 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Rock') {
+				this.debug('Rocky Payload 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		flags: {},
+		name: "Rocky payload",
+		rating: 2,
+		num: 276,
+	},
+	steelworker: {
+		inherit: false,
+		onModifyAtkPriority: 5,
+		onModifyAtk(atk, attacker, defender, move) {
+			if (move.type === 'Steel' && source.hasType('Steel')) {
+				this.debug('Steelworker 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Steel') {
+				this.debug('Steelworker 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA(atk, attacker, defender, move) {
+			if (move.type === 'Steel' && source.hasType('Steel')) {
+				this.debug('Steelworker 1.3 boost');
+				return this.chainModify(1.3);
+			}
+			else if (move.type === 'Steel') {
+				this.debug('Steelworker 1.5 boost');
+				return this.chainModify(1.5);
+			}
+		},
+		flags: {},
+		name: "Steelworker",
+		rating: 2,
+		num: 200,
+	},
 };
