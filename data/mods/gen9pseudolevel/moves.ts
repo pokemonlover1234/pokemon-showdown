a9ec637fcac96e15799a716c00269556950ab8d1
Miracle-Bee <missroleplaygamer@gmail.com>
2026-06-26 11:49:50 -0400

    Update moves.ts

--- data/mods/gen9pseudolevel/moves.ts
+++ data/mods/gen9pseudolevel/moves.ts
@@ -3,4 +3,33 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		inherit: true,
 		selfSwitch: true,
 	},
+	darkvoid: {
+		inherit: false,
+		num: 464,
+		accuracy: 50,
+		basePower: 0,
+		category: "Status",
+		name: "Dark Void",
+		pp: 10,
+		priority: 0,
+		flags: { protect: 1, reflectable: 1, mirror: 1, metronome: 1},
+		status: 'slp',
+		onModifyMove(move, pokemon, target) {
+			switch (target?.effectiveWeather()) {
+			case 'night time':
+				move.accuracy = true;
+				break;
+		}
+		onTry(source, target, move) {
+			if (move.hasBounced) {
+				return;
+			}
+			this.add('-fail', source, 'move: Dark Void');
+			return null;
+		},
+		target: "allAdjacentFoes",
+		type: "Dark",
+		zMove: { effect: 'clearnegativeboost' },
+		contestType: "Clever",
+	},
 };
